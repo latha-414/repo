37b78cbc086b4125c7b44121736e1563f16f2179
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-19 15:20:29 +0530

    Update main.tf

--- appreg/main.tf
+++ appreg/main.tf
@@ -10,7 +10,7 @@ resource "azuread_application" "my_app" {
 
 # Optional: Create Service Principal
 resource "azuread_service_principal" "my_app_sp" {
-  application_id = azuread_application.my_app.application_id
+  client_id = azuread_application.my_app.application_id
 }
 
 # Create User
